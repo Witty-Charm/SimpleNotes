3017106b19a2b93ea0480e8dba0d004d12410254
Jolly-Bell <91386898+Jolly-Bell@users.noreply.github.com>
2025-12-22 16:50:45 +0300

    Finaul UI polish: improved button layout and card design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,18 @@
+import org.gradle.kotlin.dsl.implementation
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
-    namespace = "com.example.calculatorcompose"
+    namespace = "com.example.simplenotes"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.calculatorcompose"
+        applicationId = "com.example.simplenotes"
         minSdk = 35
         targetSdk = 36
         versionCode = 1
@@ -31,9 +34,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+
     buildFeatures {
         compose = true
     }
@@ -42,11 +43,12 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
+    implementation("androidx.compose.material:material-icons-extended")
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
@@ -57,4 +59,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation ("androidx.room:room-runtime:2.8.4")
+    implementation ("androidx.room:room-ktx:2.8.4")
+    kapt("androidx.room:room-compiler:2.8.4")
+
 }
--- app/src/main/java/com/example/calculatorcompose/Note.kt
+++
@@ -1,6 +0,0 @@
-package com.example.calculatorcompose
-
-data class Note(
-    val name: String,
-    val isDone: Boolean = false
-)
